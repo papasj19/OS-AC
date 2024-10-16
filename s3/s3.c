b5414d027430d6a5dab182356ad14e5b3180fdc9
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2024-10-16 19:44:05 +0200

    threads joined

--- s3/s3.c
+++ s3/s3.c
@@ -17,6 +17,133 @@
 #define customWrite(x) write(1, x, strlen(x))
 
 
+void * doMean(void * arg) {    
+    float* info = (float *) arg; // Initialize max to the first element of the array
+    float sum = 0;
+    float *returnable = malloc(1 * sizeof(float));
+    for (int i = 0; i < sizeof(info); i++) {
+        sum += info[i];
+    }
+    returnable[0] = sum / sizeof(info);
+    return (void *) returnable;
+}
+
+
+int compare(const void *a, const void *b) {
+    return (*(float*)a - *(float*)b);
+}
+
+void *doMedian(void *arg) {
+    // Cast the void pointer to a float pointer
+    float* info = (float *) arg;
+
+    // First element contains the size of the array (assuming you pass the size in the array)
+    int size = (int)info[0];  // info[0] stores the size of the array
+
+    // Allocate memory for a copy of the array to sort
+    float* arrCopy = malloc(size * sizeof(float));
+    if (arrCopy == NULL) {
+        perror("Malloc failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Copy the original array elements to arrCopy
+    for (int i = 0; i < size; i++) {
+        arrCopy[i] = info[i + 1];
+    }
+
+    // Sort the copied array
+    qsort(arrCopy, size, sizeof(float), compare);
+
+    // Calculate the median
+    float median;
+    if (size % 2 != 0) {
+        median = arrCopy[size / 2];  // Odd number of elements
+    } else {
+        median = (arrCopy[size / 2 - 1] + arrCopy[size / 2]) / 2.0;  // Even number of elements
+    }
+
+    // Allocate memory for the return value
+    float *returnable = malloc(1 * sizeof(float));
+    if (returnable == NULL) {
+        perror("Malloc failed");
+        free(arrCopy); // Free the sorted array in case of failure
+        exit(EXIT_FAILURE);
+    }
+
+    // Store the median in returnable and return
+    *returnable = median;
+
+    // Free the copied array
+    free(arrCopy);
+
+    return (void *) returnable;
+}
+
+
+
+
+void *doMax(void * arg) {
+    float* info = (float *) arg; // Initialize max to the first element of the array
+    float max = info[0];
+    float *returnable = malloc(1 * sizeof(float));
+    for (int i = 1; i < sizeof(arg); i++) {
+        if (info[i] > max) {
+            max = info[i]; 
+        }
+    }
+    *returnable = max;
+    return (void *) returnable;
+}
+
+void *doMin(void * arg){
+    float* info = (float *) arg; 
+    float max = info[0];
+    float *returnable = malloc(1 * sizeof(float));
+    for (int i = 1; i < sizeof(arg); i++) {
+        if (info[i] < max) {
+            max = info[i]; 
+        }
+    }
+    *returnable = max;
+    return (void *) returnable;
+}
+
+void *doVariance(void *arg) {
+    // Cast the void pointer to a float pointer
+    float* info = (float *) arg;
+
+    // First element contains the size of the array (assuming you pass size in the array)
+    int size = (int)info[0];  // info[0] stores the size of the array
+
+    // Calculate the mean
+    float sum = 0;
+    for (int i = 1; i <= size; i++) {
+        sum += info[i];
+    }
+    float mean = sum / size;
+
+    // Calculate the variance
+    float variance = 0;
+    for (int i = 1; i <= size; i++) {
+        variance += pow((info[i] - mean), 2);
+    }
+    variance /= size;
+
+    // Allocate memory for the return value
+    float *returnable = malloc(1 * sizeof(float));
+    if (returnable == NULL) {
+        perror("Malloc failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Store the variance in returnable and return
+    *returnable = variance;
+    return (void *) returnable;
+}
+
+
+
 // used to read from file
 char *read_until(int fd, char end) {
     char *string = NULL;
@@ -75,18 +202,110 @@ int main(int argc, char *argv[]) {
 
     close(fd);  // Close the file descriptor
 
+int s1;
+    pthread_t t1;
+    s1 = pthread_create(&t1, NULL, doMean, data);
+    if(s1 != 0){
+        
+        char *buffer;
+        asprintf(&buffer, "bad creation :(\n");
+        write(1, buffer, strlen(buffer));
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+
+    int s2;
+    pthread_t t2;
+    s2 = pthread_create(&t2, NULL, doMedian, data);
+    if(s2 != 0){
+        char *buffer;
+        asprintf(&buffer, "bad creation :(\n");
+        write(1, buffer, strlen(buffer));
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+
+    int s3;
+    pthread_t t3;
+    s3 = pthread_create(&t3, NULL, doMax, data);
+    if(s3 != 0){
+        char *buffer;
+        asprintf(&buffer, "bad creation :(\n");
+        write(1, buffer, strlen(buffer));
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+
+    int s4;
+    pthread_t t4;
+    s4 = pthread_create(&t4, NULL, doMin, data);
+    if(s4 != 0){
+        char *buffer;
+        asprintf(&buffer, "bad creation :(\n");
+        write(1, buffer, strlen(buffer));
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+
+    int s5;
+    pthread_t t5;
+    s5 = pthread_create(&t5, NULL, doVariance, data);
+    if(s5 != 0){
+        char *buffer;
+        asprintf(&buffer, "bad creation :(\n");
+        write(1, buffer, strlen(buffer));
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+
+    void *res1;
+    void *res2;
+    void *res3;
+    void *res4;
+    void *res5;
+
+    
 
 
 
+   //Make sure join is fine 
+    s1 = pthread_join(t1, &res1); 
+    if(s1 != 0){
+        printf("pthread_join\n");
+        exit (EXIT_FAILURE); 
+    }
 
+    s2 = pthread_join(t2, &res2); 
+    if(s2 != 0){
+        printf("pthread_join\n");
+        exit (EXIT_FAILURE); 
+    }
 
+        s3 = pthread_join(t3, &res3); 
+    if(s3 != 0){
+        printf("pthread_join\n");
+        exit (EXIT_FAILURE); 
+    }
 
+        s4 = pthread_join(t4, &res4); 
+    if(s4 != 0){
+        printf("pthread_join\n");
+        exit (EXIT_FAILURE); 
+    }
 
-
+        s5 = pthread_join(t5, &res5); 
+    if(s5 != 0){
+        printf("pthread_join\n");
+        exit (EXIT_FAILURE); 
+    }
 
 
 	// DO NOT REMOVE!! WE NEED TO FREEEEEEE
 	free(data);
+
+
+    exit (EXIT_SUCCESS);
+
 
     return 0;
 }
