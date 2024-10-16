98c5a8b682b26defd928d678e6cbbb57d56c433b
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2024-10-16 19:28:39 +0200

    functions done

--- s3/j.c
+++ s3/j.c
@@ -10,38 +10,135 @@
 #include <string.h>
 
 
-int y;
+void * doMean(void *arg){
 
-void *threadFunc (void *arg) {
-    int *number = (int *)arg;
-    *number = *number + 5;
-    y = y + 1;
-    printf("From thread y=%d\n", y);
+
+
     return (void *) arg; 
 }
 
+void doMedian(){
 
-int main (){
+}
+
+
+
+void *doMax(void * arg) {
+    float* info = (float *) arg; // Initialize max to the first element of the array
+    float max = info[0];
+    float *returnable = malloc(1 * sizeof(float));
+    for (int i = 1; i < sizeof(arg); i++) {
+        if (info[i] > max) {
+            max = info[i]; 
+        }
+    }
+    *returnable = max;
+    return (void *) returnable;
+}
+
+void *doMin(void * arg){
+    float* info = (float *) arg; 
+    float max = info[0];
+    float *returnable = malloc(1 * sizeof(float));
+    for (int i = 1; i < sizeof(arg); i++) {
+        if (info[i] < max) {
+            max = info[i]; 
+        }
+    }
+    *returnable = max;
+    return (void *) returnable;
+}
+
+void doVar(){
+
+}
+
+
+int main ( int argc, char* argv[]){
+
+
+    float info[100];
+
+    if (argc != 2) {
+        char *buffer;
+        asprintf(&buffer, "Arguments wrong.\n");
+        write(1, buffer, strlen(buffer));
+        free(buffer);
+    }
+
+    int s1;
     pthread_t t1;
-    void *res; int s, x;
-    x = 7;
-    y = 5;
-    printf("[main] Initial value x=%d\n", x); 
-    printf("[main] Initial value y=%d\n", y);
-    s = pthread_create(&t1, NULL, threadFunc, &x);
-    if(s != 0){
-        printf("pthread_create\n");
+    s1 = pthread_create(&t1, NULL, doMean, NULL);
+    if(s1 != 0){
+        
+        char *buffer;
+        asprintf(&buffer, "bad creation :(\n");
+        write(1, buffer, strlen(buffer));
+        free(buffer);
         exit(EXIT_FAILURE);
     }
-    y = y + 2;
-    printf("[main] Middle value y=%d\n", y);
+
+    int s2;
+    pthread_t t2;
+    s2 = pthread_create(&t2, NULL, doMedian, NULL);
+    if(s2 != 0){
+        char *buffer;
+        asprintf(&buffer, "bad creation :(\n");
+        write(1, buffer, strlen(buffer));
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+
+    int s3;
+    pthread_t t3;
+    s3 = pthread_create(&t3, NULL, doMax, info);
+    if(s3 != 0){
+        char *buffer;
+        asprintf(&buffer, "bad creation :(\n");
+        write(1, buffer, strlen(buffer));
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+
+    int s4;
+    pthread_t t4;
+    s4 = pthread_create(&t4, NULL, doMin, NULL);
+    if(s4 != 0){
+        char *buffer;
+        asprintf(&buffer, "bad creation :(\n");
+        write(1, buffer, strlen(buffer));
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+
+    int s5;
+    pthread_t t5;
+    s5 = pthread_create(&t5, NULL, doVar, NULL);
+    if(s5 != 0){
+        char *buffer;
+        asprintf(&buffer, "bad creation :(\n");
+        write(1, buffer, strlen(buffer));
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+
+    void *res1;
+    void *res2;
+    void *res3;
+    void *res4;
+    void *res5;
+
+    
+
+
+
+   //Make sure join is fine 
     s = pthread_join(t1, &res); 
     if(s != 0){
         printf("pthread_join\n");
         exit (EXIT_FAILURE); 
     }
-    printf("Thread returned %lX\n", (long)res); 
-    printf("[main] Final value x=%d\n", x); 
-    printf("[main] Final value y=%d\n", y);
+
+
     exit (EXIT_SUCCESS);
 }
