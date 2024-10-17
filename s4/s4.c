0b182ce11dc48321308425bc11edbe99e87dd914
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2024-10-17 12:48:37 +0200

    setting layout for next week

---
+++ s4/s4.c
@@ -0,0 +1,31 @@
+// Operating Systems Lab - Session 4
+// Guillermo Nebra Aljama <guillermo.nebra>
+// Spencer Johnson <spencerjames.johnson>
+
+
+#define _GNU_SOURCE
+#define _XOPEN_SOURCE 500
+#define _POSIX_C_SOURCE 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <math.h>
+
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        customWrite("Usage: ./g <file>\n");
+        return 1;
+    }
+
+
+    
+
+
+    return 0; 
+}
