ba4d84bf649bb2123b5c1bdde8566481e93c0a64
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2024-10-23 12:58:44 +0200

    playing around  before lab

--- s4/serv.c
+++ s4/serv.c
@@ -36,7 +36,9 @@ int main(int argc, char *argv[]) {
 
     struct sockaddr_in servaddr;
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(9656);
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    
     
 
 
