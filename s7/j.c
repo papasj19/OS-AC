f55f316635060dc36d22a67d2944a5d81df808b8
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2024-11-20 20:19:55 +0100

    closer

--- s7/j.c
+++ s7/j.c
@@ -46,6 +46,14 @@
 #define printF(x) write(1, x, strlen(x))
 
 
+typedef struct {
+    char *key;
+    char *value;
+} Entry;
+
+Entry * dictionary; 
+int dictionary_size = 0;
+
 char *read_until(int fd, char end)
 {
     char *string = NULL;
@@ -72,6 +80,119 @@ char *read_until(int fd, char end)
     string[i] = '\0';
     return string;
 }
+
+
+void parse_dictionary(const char *filename) {
+    int fd = open(filename, O_RDONLY);
+    if (fd < 0) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the first line to get the number of entries
+    char *line = read_until(fd, '\n');
+    int num_entries = atoi(line);
+    free(line);
+
+    dictionary_size = num_entries;
+
+    // Allocate memory for storing the entries
+    dictionary = malloc(num_entries * sizeof(Entry));
+    if (!dictionary) {
+        perror("Memory allocation failed");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Parse each line into key-value pairs
+    for (int i = 0; i < num_entries; i++) {
+        line = read_until(fd, '\n');
+
+        // Locate the delimiter ':'
+        char *delimiter_pos = strchr(line, ':');
+        if (delimiter_pos == NULL) {
+            fprintf(stderr, "Invalid line format: %s\n", line);
+            free(line);
+            continue;
+        }
+
+        // Compute lengths of key and value
+        size_t key_length = delimiter_pos - line;
+        size_t value_length = strlen(line) - key_length - 1;
+
+        // Allocate memory for key and value
+        dictionary[i].key = malloc(key_length + 1);
+        dictionary[i].value = malloc(value_length + 1);
+
+        if (!dictionary[i].key || !dictionary[i].value) {
+            perror("Memory allocation failed");
+            free(line);
+            continue;
+        }
+
+        // Copy key and value into allocated memory using strcpy
+        strncpy(dictionary[i].key, line, key_length);
+        dictionary[i].key[key_length] = '\0';  // Null-terminate the key
+        strcpy(dictionary[i].value, delimiter_pos + 1);
+
+        // Print the result
+        printf("Key: %s\nValue: %s\n\n", dictionary[i].key, dictionary[i].value);
+
+        free(line);
+    }
+
+    // Free allocated memory
+    for (int i = 0; i < num_entries; i++) {
+        free(dictionary[i].key);
+        free(dictionary[i].value);
+    }
+    free(dictionary);
+
+    close(fd);
+}
+
+int search_word(const char *word) {
+    for (int i = 0; i < dictionary_size; i++) {
+        if (strcmp(dictionary[i].word, word) == 0) {
+            return i;  // Return the index of the word
+        }
+    }
+    return -1;  // Word not found
+}
+
+// Function to handle a query
+void handle_query(const char *word) {
+    int index = search_word(word);
+    if (index == -1) {
+        printf("E* The word %s has not been found in the dictionary.\n", word);
+    } else {
+        printf("D*%s*%s\n", dictionary[index].word, dictionary[index].definition);
+    }
+}
+
+// Function to handle adding a new word
+void handle_add(const char *word, const char *definition) {
+    if (search_word(word) != -1) {
+        printf("E* The word %s is already in the dictionary.\n", word);
+        return;
+    }
+    // Add the word and definition
+    dictionary[dictionary_size].word = strdup(word);
+    dictionary[dictionary_size].definition = strdup(definition);
+    dictionary_size++;
+    printf("OK* The word %s has been added to the dictionary.\n", word);
+}
+
+// Function to handle listing all words
+void handle_list() {
+    printf("L*%d\n", dictionary_size);
+    for (int i = 0; i < dictionary_size; i++) {
+        printf("%s\n", dictionary[i].word);
+    }
+}
+
+
+
 
 
 void newUser(char* username){
@@ -123,34 +244,42 @@ void doSearchWord(int connectFD){
 
 
 
+
+
 //useee 
 int do_connection(int connectFD){
 
     char *username = read_until(connectFD, '\n');
     newUser(username);
-
+    int operation = 0;
 
     while(1){
 
         char *choice = read_until(connectFD, '\n');
 
+        if (choice[1] != '*') {
+            printf("E* Invalid frame format.\n");
+        return;
+    }
+    operation = choice[0];
+
         switch (atoi(choice))
         {
-            case 1:
+            case 'C':
                 doSearchWord(connectFD);
 
             break;
 
-            case 2:
+            case 'A':
                 printF("\nUser has requested add word command\n");
 
             break;
 
-            case 3:
+            case 'L':
                 printF("\nUser has requested list words command\n");
             break;
 
-            case 4:
+            case '':
                 return 100; 
             break;
         
@@ -171,6 +300,8 @@ int main(int argc, char *argv[]) {
         printF("\nError: Number of args is not 2. It is neccessary to provide an IP, port and filename\n");
         return 1;
     }
+
+    parse_dictionary(argv[3]);
 
     printf("Dictionary Server Started\n");
 
