9b49c530590f807676e538601483afdbac58f2b1
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2024-11-25 15:27:25 +0100

    setting up for wednesday

---
+++ s8/j.c
@@ -0,0 +1,76 @@
+/* 
+    Operating Systems: Lab 7: Select
+    
+    Developed by:
+        - Guillermo Nebra Aljama    <guillermo.nebra>
+        - Spencer Johnson           <spencerjames.johnson>
+    Developed on: November 20th, 2024
+    :)
+*/
+
+#define printF(x) write(1, x, strlen(x))
+
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <time.h>
+#include <string.h>
+
+#define NUM_STATIONS 3
+#define NUM_EVENTS 9
+
+#define ARRIVAL_REPORT 1
+#define DEPARTURE_REPORT 2
+#define END_REPORT 3
+
+#define RESET "\x1b[0m"
+#define YELLOW "\x1b[33m"
+#define BLUE "\x1b[34m"
+#define GREEN "\x1b[32m"
+
+#define printF(x) write(1, x, strlen(x))
+
+
+char *read_until(int fd, char end)
+{
+    char *string = NULL;
+    char c;
+    int i = 0, size;
+
+    while (1)
+    {
+        size = read(fd, &c, sizeof(char));
+        if (string == NULL)
+        {
+            string = (char *)malloc(sizeof(char));
+        }
+        if (c != end && size > 0)
+        {
+            string = (char *)realloc(string, sizeof(char) * (i + 2));
+            string[i++] = c;
+        }
+        else
+        {
+            break;
+        }
+    }
+    string[i] = '\0';
+    return string;
+}
+
+
+int main(void){
+
+
+    printF("Hello world");
+
+
+    return 0; 
+}
