32f45c7442b6953c4d6d3584bab628f609868af4
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2024-11-25 16:08:30 +0100

    formatting with message queue

--- s8/j.c
+++ s8/j.c
@@ -37,6 +37,17 @@
 
 #define printF(x) write(1, x, strlen(x))
 
+typedef struct{
+    long id_msg;
+    int n1,n2; 
+}Message1;
+
+
+typedef struct{
+    long id_msg;
+    int result; 
+} Message2;
+
 
 char *read_until(int fd, char end)
 {
@@ -66,11 +77,75 @@ char *read_until(int fd, char end)
 }
 
 
-int main(void){
+void doSomething(int messageNumber){
+
+    switch(messageNumber){
+        case 1:
+            printF("Enter num1:");
+            break;
+        case 2:
+            printF("Enter num2:");
+            break;
+        case 3:
+            printF("Sending petition...\n");
+            break;
+        case 4:
+            printF("Program finished\n\n");
+            break;
+        case 5:
+            printF("Received: ");
+            break;
+        default:
+            break;
+    }
+}
 
 
-    printF("Hello world");
+
+int main(){
+    key_t key; int id_queue; Message1 M1; Message2 M2;
 
 
-    return 0; 
+    // file and integer (must be the same for all )
+    key = ftok("EnterNumbers.c", 12);
+    if (key == (key_t)-1){ 
+        printf("Error: Key\n\n"); exit(-1);
+    }
+
+    //create the queue
+    id_queue = msgget(key, 0600|IPC_CREAT);
+    if(id_queue == -1){
+        printf("Error creating queue\n\n"); exit(-1);
+    }
+
+    M1.id_msg = 1;
+    M1.n1 = 1;
+    
+    while(M1.n1 != 0){
+
+        //rec info
+        printf("Enter num1:");
+        scanf("%d", &M1.n1);
+        printf("Enter num2:");
+        scanf("%d", &M1.n2);
+
+        //send info in message struct 
+        msgsnd(id_queue, (struct msgbuf *)&M1, sizeof(int)*2,IPC_NOWAIT);
+        printf("Sending petition...\n"); 
+        if (M1.n1 != 0){
+
+            //rec message from other process 
+            msgrcv(id_queue, (struct msgbuf *)&M2, sizeof(int), 2, 0);
+            printf("%d+%d = %d\n\n", M1.n1, M1.n2, M2.result); 
+
+            //do somethign with result 
+            doSomething(M2.result);
+        }
+    }
+    printf("Program finished\n\n");
+
+    //close queue
+    msgctl(id_queue, IPC_RMID, (struct msqid_ds *)NULL);
+
+    return 0;
 }
