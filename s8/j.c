609d54a5ff1be5b775102e0c9db32ba408c80c88
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2024-11-27 19:45:56 +0100

    should be somewhat okay

--- s8/j.c
+++ s8/j.c
@@ -23,12 +23,16 @@
 #include <time.h>
 #include <string.h>
 
-#define NUM_STATIONS 3
-#define NUM_EVENTS 9
+#define REQUEST_TIMES 1
+#define RESERVE 2
+#define CONFIRMED 3
+#define NOT_AVAILABLE 4
 
-#define ARRIVAL_REPORT 1
-#define DEPARTURE_REPORT 2
-#define END_REPORT 3
+#define TOT 8
+
+const char *time_slots[TOT] = {"09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"};
+int bookings[TOT] = {0};
+
 
 #define RESET "\x1b[0m"
 #define YELLOW "\x1b[33m"
@@ -47,6 +51,12 @@ typedef struct{
     long id_msg;
     int result; 
 } Message2;
+
+struct msg {
+    long mtype;
+    char header[20];
+    char data[256];
+};
 
 
 char *read_until(int fd, char end)
@@ -77,37 +87,125 @@ char *read_until(int fd, char end)
 }
 
 
-void doSomething(int messageNumber){
+void load_appointments(const char *filename) {
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        return; 
+    }
+    if (read(fd, bookings, sizeof(bookings)) != sizeof(bookings)) {
+        perror("Error reading file");
+    }
+    close(fd);
+}
 
-    switch(messageNumber){
-        case 1:
-            printF("Enter num1:");
+
+void save_appointments(const char *filename) {
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if (fd == -1) {
+        perror("Error opening file");
+        return;
+    }
+    if (write(fd, bookings, sizeof(bookings)) != sizeof(bookings)) {
+        perror("Error writing file");
+    }
+    close(fd);
+}
+
+void reserveTime(struct msg *mess, int queue_num){
+    printF("Reservation request received.\n");
+    int slot_found = -1;
+
+    for (int i = 0; i < TOT; i++) {
+        if (strcmp(mess->data, time_slots[i]) == 0) {
+            slot_found = i;
             break;
-        case 2:
-            printF("Enter num2:");
-            break;
-        case 3:
-            printF("Sending petition...\n");
-            break;
-        case 4:
-            printF("Program finished\n\n");
-            break;
-        case 5:
-            printF("Received: ");
-            break;
-        default:
-            break;
+        }
+    }
+
+    if (slot_found != -1 && bookings[slot_found] < 2) {
+        bookings[slot_found]++;
+
+
+           char *reservation_info = NULL;
+        asprintf(&reservation_info, "Person %s requested appointment at %s.\n", 
+                 mess->header, time_slots[slot_found]);
+        char *reserved_msg = NULL;
+        asprintf(&reserved_msg, "Time reserved.\n");
+
+        // Output the dynamically created strings
+        if (reservation_info) {
+            printF(reservation_info);
+            free(reservation_info);
+        }
+        if (reserved_msg) {
+            printF(reserved_msg);
+            free(reserved_msg);
+        }
+
+
+
+        mess->mtype = CONFIRMED;
+        strcpy(mess->data, "CONFIRMED");
+    } else {
+        mess->mtype = NOT_AVAILABLE;
+        strcpy(mess->data, "NOT_AVAILABLE");
+    }
+
+    if (msgsnd(queue_num, mess, sizeof(*mess) - sizeof(long), 0) == -1) {
+        perror("Error sending response");
     }
 }
+
+void requestTimes(struct msg *message, int queueNum) {
+    printF("Requesting available times...\n");
+    char response[256] = "";
+
+    for (int i = 0; i < TOT; i++) {
+        if (bookings[i] < 2) { 
+            strcat(response, time_slots[i]);
+            strcat(response, " ");
+        }
+    }
+    message->mtype = REQUEST_TIMES;
+    strcpy(message->data, response);
+
+    if (msgsnd(queueNum, message, sizeof(*message) - sizeof(long), 0) == -1) {
+        perror("Error sending response");
+    }
+}
+
+
+void doThings(int queue_num){
+    struct msg message;
+
+    while (1) {
+        if (msgrcv(queue_num, &message, sizeof(message) - sizeof(long), 0, 0) == -1) {
+            perror("Error receiving message");
+            continue;
+        }
+
+        if (strcmp(message.header, "REQUEST_TIMES") == 0) {
+            requestTimes(&message, queue_num);
+        } else if (strcmp(message.header, "RESERVE") == 0) {
+            reserveTime(&message, queue_num);
+        }
+    }
+}
+
 
 
 
 int main(){
     key_t key; int id_queue; Message1 M1; Message2 M2;
 
+    printF("Adminstration process started\n");
 
-    // file and integer (must be the same for all )
-    key = ftok("EnterNumbers.c", 12);
+    printF("Requesting available times...\n");
+
+    load_appointments("appointments.dat");
+
+    key = ftok("S8_administration.c", 12);
     if (key == (key_t)-1){ 
         printf("Error: Key\n\n"); exit(-1);
     }
@@ -118,31 +216,11 @@ int main(){
         printf("Error creating queue\n\n"); exit(-1);
     }
 
-    M1.id_msg = 1;
-    M1.n1 = 1;
-    
-    while(M1.n1 != 0){
 
-        //rec info
-        printf("Enter num1:");
-        scanf("%d", &M1.n1);
-        printf("Enter num2:");
-        scanf("%d", &M1.n2);
+    doThings(id_queue);
 
-        //send info in message struct 
-        msgsnd(id_queue, (struct msgbuf *)&M1, sizeof(int)*2,IPC_NOWAIT);
-        printf("Sending petition...\n"); 
-        if (M1.n1 != 0){
 
-            //rec message from other process 
-            msgrcv(id_queue, (struct msgbuf *)&M2, sizeof(int), 2, 0);
-            printf("%d+%d = %d\n\n", M1.n1, M1.n2, M2.result); 
 
-            //do somethign with result 
-            doSomething(M2.result);
-        }
-    }
-    printf("Program finished\n\n");
 
     //close queue
     msgctl(id_queue, IPC_RMID, (struct msqid_ds *)NULL);
