bc2de6a3c57754f30c8d8f15d43953c870381223
guillermonebra <91937135+guillermonebra@users.noreply.github.com>
2024-12-11 19:25:14 +0100

    substituting calloc with malloc+memset. removing comments

--- s10/g.c
+++ s10/g.c
@@ -15,7 +15,6 @@
 #define NUM_STATIONS 5
 #define MAX_CARS_IN_PROCESS 2
 
-// Messages
 #define ERR_ARGS "Usage: ./S10.exe <number of cars>\n"
 #define STATION_START "\033[0;32m[%s] of car [%d] starting\033[0m\n"
 #define STATION_COMPLETE "\033[0;34m[%s] of car [%d] assembled\033[0m\n"
@@ -23,29 +22,24 @@
 #define QC_STATUS "\033[0;%dm[%s]\033[0m\n"
 #define QC_PASSED "\033[0;32mQuality control of car [%d] passed\033[0m\n"
 
-// Station names
 const char* STATION_NAMES[] = {"Body Station", "Engine Station", "Wheel Station", "Paint Station", "Quality Control"};
 
-// Semaphores for synchronization
-semaphore sem_screen;          // For screen output
-semaphore sem_process_limit;   // Limit concurrent cars
-semaphore sem_stations[NUM_STATIONS];  // For each station
+semaphore sem_screen;
+semaphore sem_process_limit;
+semaphore sem_stations[NUM_STATIONS];
 pthread_t* station_threads;
 
-// Shared state
 typedef struct {
     int num_cars;
-    int* car_station;     // Current station for each car
-    int* car_complete;    // Track completed cars
-    int total_complete;   // Total completed cars
+    int* car_station;
+    int* car_complete;
+    int total_complete;
 } AssemblyState;
 
 AssemblyState state;
 char* aux;
 
-// Initialize semaphores
 void init_semaphores() {
-    // Create semaphores with unique keys
     SEM_constructor_with_name(&sem_screen, 1000);
     SEM_constructor_with_name(&sem_process_limit, 1001);
     
@@ -53,7 +47,6 @@ void init_semaphores() {
         SEM_constructor_with_name(&sem_stations[i], 1002 + i);
     }
     
-    // Initialize semaphores
     SEM_init(&sem_screen, 1);
     SEM_init(&sem_process_limit, MAX_CARS_IN_PROCESS);
     
@@ -62,7 +55,6 @@ void init_semaphores() {
     }
 }
 
-// Cleanup semaphores
 void cleanup_semaphores() {
     SEM_destructor(&sem_screen);
     SEM_destructor(&sem_process_limit);
@@ -71,12 +63,10 @@ void cleanup_semaphores() {
     }
 }
 
-// Random time between 1-5 seconds
 int get_random_time() {
     return (rand() % 5) + 1;
 }
 
-// Simulates work at a station
 void do_station_work(int car_num, int station_idx) {
     int work_time = get_random_time();
     
@@ -95,12 +85,10 @@ void do_station_work(int car_num, int station_idx) {
     SEM_signal(&sem_screen);
 }
 
-// Quality control check for a specific aspect
 int check_quality() {
     return (rand() % 10) > 5;
 }
 
-// Handle quality control station
 void do_quality_control(int car_num) {
     const char* aspects[] = {"Body", "Engine", "Wheels", "Paint"};
     int all_passed;
@@ -134,7 +122,6 @@ void do_quality_control(int car_num) {
     SEM_signal(&sem_screen);
 }
 
-// Station thread function
 void* station_worker(void* arg) {
     int station_idx = *(int*)arg;
     free(arg);
@@ -142,27 +129,23 @@ void* station_worker(void* arg) {
     while(state.total_complete < state.num_cars) {
         SEM_wait(&sem_stations[station_idx]);
         
-        // Process cars at this station
         for(int car = 0; car < state.num_cars; car++) {
             if(state.car_complete[car]) continue;
             
             if(state.car_station[car] == station_idx) {
-                // Do station work
                 if(station_idx == NUM_STATIONS - 1) {
                     do_quality_control(car);
                 } else {
                     do_station_work(car, station_idx);
                 }
                 
-                // Update car state
                 state.car_station[car]++;
                 if(state.car_station[car] == NUM_STATIONS) {
                     state.car_complete[car] = 1;
-                    SEM_signal(&sem_process_limit);  // Allow new car to enter
+                    SEM_signal(&sem_process_limit);
                     state.total_complete++;
                 }
                 
-                // Signal next station
                 if(state.car_station[car] < NUM_STATIONS) {
                     SEM_signal(&sem_stations[state.car_station[car]]);
                 }
@@ -170,7 +153,7 @@ void* station_worker(void* arg) {
         }
         
         SEM_signal(&sem_stations[station_idx]);
-        usleep(100000); // Small delay to prevent busy waiting
+        usleep(100000);
     }
     
     return NULL;
@@ -188,18 +171,16 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     
-    // Initialize state
-    state.car_station = calloc(state.num_cars, sizeof(int));
-    state.car_complete = calloc(state.num_cars, sizeof(int));
+    state.car_station = malloc(state.num_cars * sizeof(int));
+    memset(state.car_station, 0, state.num_cars * sizeof(int));
+    state.car_complete = malloc(state.num_cars * sizeof(int));
+    memset(state.car_complete, 0, state.num_cars * sizeof(int));
     state.total_complete = 0;
     
-    // Initialize random seed
     srand(time(NULL));
     
-    // Initialize semaphores
     init_semaphores();
     
-    // Create station threads
     station_threads = malloc(NUM_STATIONS * sizeof(pthread_t));
     for(int i = 0; i < NUM_STATIONS; i++) {
         int* station_idx = malloc(sizeof(int));
@@ -207,19 +188,16 @@ int main(int argc, char *argv[]) {
         pthread_create(&station_threads[i], NULL, station_worker, station_idx);
     }
     
-    // Start cars (max 2 at a time)
     for(int car = 0; car < state.num_cars; car++) {
         SEM_wait(&sem_process_limit);
         state.car_station[car] = 0;
-        SEM_signal(&sem_stations[0]);  // Signal first station
+        SEM_signal(&sem_stations[0]);
     }
     
-    // Wait for all stations to complete
     for(int i = 0; i < NUM_STATIONS; i++) {
         pthread_join(station_threads[i], NULL);
     }
     
-    // Cleanup
     cleanup_semaphores();
     free(station_threads);
     free(state.car_station);
